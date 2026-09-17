94dabba0a5c4eaede7f6a48d38e87a8972fb065f
Kristalynn122523-design <kristalynn122523@gmail.com>
2026-09-17 14:49:38 -0500

    update

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const menu = document.getElementById("menu")
 
 
 function openMenu() {
-  menu.classList.add("menu--open");
+  menu.classList.add(" menu--open");
 }
 
 function closeMenu() {
-  menu.classList.remove("menu--open");
+  menu.classList.remove(" menu--open");
 }
